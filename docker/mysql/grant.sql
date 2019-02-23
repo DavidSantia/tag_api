76b4866a3357dfd98aeb74c1381da1aa3545b574
David Santia <davidsantia.morris@gmail.com>
2019-02-22 17:57:45 -0700

    Add APM user for monitoring MySQL

--- docker/mysql/grant.sql
+++ docker/mysql/grant.sql
@@ -6,3 +6,11 @@ CREATE USER 'demo'@'localhost' IDENTIFIED BY 'welcome1';
 GRANT SELECT, INSERT, UPDATE, DELETE, LOCK TABLES, EXECUTE ON *.* TO 'demo'@'localhost';
 CREATE USER 'demo'@'%' IDENTIFIED BY 'welcome1';
 GRANT SELECT, INSERT, UPDATE, DELETE, LOCK TABLES, EXECUTE ON *.* TO 'demo'@'%';
+
+--
+-- Add APM user
+--
+
+CREATE USER 'newrelic'@'localhost' IDENTIFIED BY 'welcome1';
+GRANT REPLICATION CLIENT ON *.* TO 'newrelic'@'localhost' WITH MAX_USER_CONNECTIONS 5;
+
