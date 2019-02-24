971501755b8d05c61402d3d9d5255a7fbb281bf8
David Santia <davidsantia.morris@gmail.com>
2019-02-23 17:52:05 -0700

    Update MySQL init to allow APM user to be on any IP

--- docker/mysql/grant.sql
+++ docker/mysql/grant.sql
@@ -2,8 +2,6 @@
 -- Add demo user
 --
 
-CREATE USER 'demo'@'localhost' IDENTIFIED BY 'welcome1';
-GRANT SELECT, INSERT, UPDATE, DELETE, LOCK TABLES, EXECUTE ON *.* TO 'demo'@'localhost';
 CREATE USER 'demo'@'%' IDENTIFIED BY 'welcome1';
 GRANT SELECT, INSERT, UPDATE, DELETE, LOCK TABLES, EXECUTE ON *.* TO 'demo'@'%';
 
@@ -11,6 +9,6 @@ GRANT SELECT, INSERT, UPDATE, DELETE, LOCK TABLES, EXECUTE ON *.* TO 'demo'@'%';
 -- Add APM user
 --
 
-CREATE USER 'newrelic'@'localhost' IDENTIFIED BY 'welcome1';
-GRANT REPLICATION CLIENT ON *.* TO 'newrelic'@'localhost' WITH MAX_USER_CONNECTIONS 5;
+CREATE USER 'newrelic'@'%' IDENTIFIED BY 'welcome1';
+GRANT REPLICATION CLIENT ON *.* TO 'newrelic'@'%' WITH MAX_USER_CONNECTIONS 5;
 
